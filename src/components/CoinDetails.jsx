25e974f3477d6f868265cb82fdd09744758fc0bd
Mohammed uzair mauzam <uzairmauzam68@gmail.com>
2023-05-30 20:13:33 +0530

    completed

--- src/components/CoinDetails.jsx
+++ src/components/CoinDetails.jsx
@@ -1,4 +1,4 @@
-import { Container, HStack, Radio, RadioGroup, Box, VStack, Text, Image, Img } from '@chakra-ui/react'
+import { Container, HStack, Radio, RadioGroup, Box, VStack, Badge, Text, Img, Stat, StatLabel, StatNumber, StatHelpText, StatArrow, Progress } from '@chakra-ui/react'
 import React, { useEffect, useState } from 'react'
 import Loader from './Loader'
 import { useParams } from 'react-router-dom'
@@ -11,6 +11,8 @@ function CoinDetails() {
   const [loading, setloading] = useState(true)
   const [error, seterror] = useState(false)
   const [currency, setCurrency] = useState('inr')
+
+  const currencySymbol = currency === 'inr' ? '₹' : currency === 'eur' ? '€' : '$'
 
   const params = useParams()
 
@@ -30,7 +32,8 @@ function CoinDetails() {
     getCoinD(params.id)
   }, [])
 
-  const currencySymbol = currency === 'inr' ? '₹' : currency === 'eur' ? '€' : '$'
+
+  if (error) return <ErrorComponent message={'Error while Fetching coins'} />
 
 
   return (
@@ -52,20 +55,70 @@ function CoinDetails() {
               </HStack>
             </RadioGroup>
 
-            <VStack spacing={'4'} p={"6"} alignItems={'flex-start'}>
+            <VStack spacing={'5'} p={"7"} alignItems={'flex-start'}>
               <Text fontSize={"small"} alignSelf={'center'} opacity={0.7}>
                 Last updated on {Date(coin.market_data.last_updated).split("G")[0]}
               </Text>
 
-              <Img src={coin.image.large} w={"16"} h={16} objectFit={"contain"}/>
+              <Img src={coin.image.large} w={"16"} h={16} objectFit={"contain"} />
+
+              <Stat>
+                <StatLabel>{coin.name}</StatLabel>
+                <StatNumber>{currencySymbol}{coin.market_data.current_price[currency]}</StatNumber>
+
+                <StatHelpText>
+                  <StatArrow type={
+                    coin.market_data.price_change_percentage_24h > 0
+                      ? 'increase'
+                      : 'decrease'} />
+                  {coin.market_data.price_change_percentage_24h}%
+                </StatHelpText>
+              </Stat>
+
+              <Badge fontSize={'2xl'} bgColor={"blackAlpha.700"} color={"white"}>{`#${coin.market_cap_rank}`}</Badge>
+
+              <CoustomBar high={`${currencySymbol}${coin.market_data.high_24h[currency]}`}
+               low={`${currencySymbol}${coin.market_data.low_24h[currency]}`}/>
+
+               <Box>
+                <Item title={"MAX SUPPLY"} value={coin.market_data.max_supply}/>
+                <Item title={"CIRCULATING SUPPLY"} value={coin.market_data.circulating_supply}/>
+                <Item title={"MARKET CAP"} value={`${currencySymbol}${coin.market_data.market_cap[currency]}`}/>
+                <Item title={"ALL TIME LOW"} value={`${currencySymbol}${coin.market_data.atl[currency]}`}/>
+                <Item title={"ALL TIME HIGH"} value={`${currencySymbol}${coin.market_data.ath[currency]}`}/>
+               </Box>
+
             </VStack>
-
-
           </>
       }
 
     </Container>
-  )
-}
+  );
+};
+
+const Item=({title,value})=>(
+<HStack justifyContent={"space-between"} w={"full"} my={'4'}>
+  <Text fontFamily={"Bebas Neue"} letterSpacing={"widest"}>
+    {title}
+  </Text>
+  <Text>
+    {value}
+  </Text>
+</HStack>
+)
+
+
+const CoustomBar = ({ high, low }) => 
+  (
+    <VStack w={"full"}>
+      <Progress value={50} colorScheme={"teal"} w={"full"} />
+      <HStack justifyContent={"space-between"} w={"full"}>
+        <Badge children={low} colorScheme={"red"} />
+        <Text fontSize={"sm"}>24H RANGE</Text>
+        <Badge children={high} colorScheme={"blue"} />
+      </HStack>
+    </VStack>
+  );
+
 
 export default CoinDetails
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,15 +3,15 @@ import React from 'react'
 import { Link } from 'react-router-dom'
 
 function Header() {
-  return <HStack p={'4'} shadow={"base"} bgColor={'blackAlpha.500'}>
+  return <HStack p={'4'} shadow={"base"} bgColor={'blackAlpha.900'}>
 
-    <Button variant={'unstyled'} color={'white'}>
+    <Button variant={'unstyled'} color={'white'} px={"6"}>
         <Link to={'/'}>Home</Link>
     </Button>
     <Button variant={'unstyled'} color={'white'}>
         <Link to={'/exchange'}>Exchanges</Link>
     </Button>
-    <Button variant={'unstyled'} color={'white'}>
+    <Button variant={'unstyled'} color={'white'} px={"6"}>
         <Link to={'/coins'}>Coins</Link>
     </Button>
   </HStack>
